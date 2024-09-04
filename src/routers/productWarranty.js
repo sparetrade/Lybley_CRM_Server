f3ee45cf48607aa7274b0ad0c5c3a69934c31d06
rajeshtiwari2503 <rajeshtiwari2503@gmail.com>
2024-09-04 13:09:41 +0530

    dhytyd

--- src/routers/productWarranty.js
+++ src/routers/productWarranty.js
@@ -3,12 +3,13 @@
 const express=require("express");
 const router=express.Router();
  
-const {addProductWarranty,activateWarranty,getAllProductWarranty,getProductWarrantyById,editProductWarranty,deleteProductWarranty }=require("../controllers/productWarrantyController")
+const {addProductWarranty,activateWarranty,getAllProductWarranty,getProductWarrantyByUniqueId,getProductWarrantyById,editProductWarranty,deleteProductWarranty }=require("../controllers/productWarrantyController")
 
 router.post("/addProductWarranty",addProductWarranty );
 router.post("/activateWarranty",activateWarranty );
 router.get("/getAllProductWarranty",getAllProductWarranty );
 router.get("/getProductWarranty/:id",getProductWarrantyById );
+router.get("/getProductWarrantyByUniqueId/:id",getProductWarrantyByUniqueId );
 router.patch("/editProductWarranty/:id",editProductWarranty );
 router.delete("/deleteProductWarranty/:id",deleteProductWarranty );
 module.exports=router;
