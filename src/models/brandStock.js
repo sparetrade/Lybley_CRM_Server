f2bbe7b20cff1585607be6943099d021689ace1c
rajeshtiwari2503 <rajeshtiwari2503@gmail.com>
2024-09-29 14:11:48 +0530

    dhhhhhdh

--- src/models/brandStock.js
+++ src/models/brandStock.js
@@ -7,6 +7,14 @@ const brandStockSchema = new mongoose.Schema({
     defectiveStock: { type: String, default: '0' },
     sparepartName:{ type: String },
     sparepartId:{ type: String },
+    stock: [
+        {
+          fresh:{type:String },
+          title:{type:String },
+          createdAt: { type: Date, default: Date.now },
+          updatedAt: { type: Date, default: Date.now },
+        },
+      ],
 }, { timestamps: true })
 
 const BrandStockModel = new mongoose.model("brandStock", brandStockSchema);
